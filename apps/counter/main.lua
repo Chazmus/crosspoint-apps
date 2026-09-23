7ce997e3773b855d6a34d46d35aea79c09aa4a79
Chaz Bailey <charles.s.bailey@gmail.com>
2026-09-23 22:07:06 +0100

    fix(counter): update to v1.0.1 with reversed button direction to match physical layout

--- apps/counter/main.lua
+++ apps/counter/main.lua
@@ -54,10 +54,10 @@ function onTouch(x, y)
 end
 
 function onInput(button, isDown)
-    if button == input.BTN_UP or button == input.BTN_CONFIRM then
+    if button == input.BTN_DOWN or button == input.BTN_CONFIRM or button == input.BTN_PAGE_FORWARD then
         count = count + 1
         saveCount()
-    elseif button == input.BTN_DOWN then
+    elseif button == input.BTN_UP or button == input.BTN_PAGE_BACK then
         count = count - 1
         saveCount()
     end
